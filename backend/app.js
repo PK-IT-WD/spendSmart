86f0371d81dd0a7207f9ad54aac3787415cdc1ac
PK-IT-WD <pradeepkumaritwd@gmail.com>
2024-12-17 17:15:05 +0530

    Removed error

--- backend/app.js
+++ backend/app.js
@@ -872,7 +872,7 @@ app.get('/previousDownload', async (req, res) => {
     }
 });
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
